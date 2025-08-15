9216d2e6cda872141241b46e8ee5ed7fc19f4ca3
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-08-15 12:08:05 +0530

    setup project

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @format */
+
+export default {
+    tabWidth: 4,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 120,
+    semi: false,
+    quoteProps: "consistent",
+    arrowParens: "avoid",
+    insertPragma: true,
+    embeddedLanguageFormatting: "off",
+    endOfLine: "lf",
+}
